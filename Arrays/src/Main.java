f5091d792e0761a590694de1a8db2b904692fef8
John Dakin <jdakin749@gmail.com>
2024-10-30 11:24:40 +0300

    Add files via upload

---
+++ Arrays/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String[] cars = new String[3];
+		
+		cars[0] = "Camero";
+		cars[1] = "Corvette";
+		cars[2] = "Tesla";
+		
+		//System.out.println(cars[2]);
+		
+		for(int i = 0; i <cars.length; i++) {
+			System.out.println(cars[i]);
+		}
+		
+	}
+
+}
